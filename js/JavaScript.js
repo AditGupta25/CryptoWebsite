9ac302d393704241241ba8e15617252bff1a727c
Adit <aditgupta1@yahoo.com>
2018-01-20 01:41:15 -0500

    Fixed footer, added possible json access fix

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -83,7 +83,7 @@ function getCrypto(){
 
 
 	// Set up and make the request.
-	request.open('GET', './data/data.json', true);
+	request.open('GET', 'https://raw.githubusercontent.com/AditGupta25/CryptoWebsite/master/data/data.json', true);
 	request.send(); 
 
 	
